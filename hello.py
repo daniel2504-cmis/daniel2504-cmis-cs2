e66aaf1db49e69e32df0f688a0765c7cd0eff867
cmis <cmis@cmis-hs01.(none)>
2016-01-28 14:35:49 +0700

    It is just hw

---
+++ hello.py
@@ -0,0 +1,22 @@
+print "Hello World"
+print "Daniel Kwon"
+print True
+print 91
+print 0.1
+print 2
+print 011
+print ("Daniel Kwon", "Pro lol", "Pro WOW", "Pro Hearth")
+Steven = "So Rude"
+print Steven
+StevenHeight = 1.4
+StevenBrain = 1
+StevenGPA = StevenHeight - StevenBrain
+print StevenGPA
+print StevenHeight
+print StevenHeight + StevenBrain
+print StevenHeight * StevenBrain
+print StevenHeight ** StevenBrain 
+print StevenHeight // StevenBrain
+print StevenBrain // StevenHeight
+print StevenBrain % StevenHeight
+print StevenHeight % StevenBrain
