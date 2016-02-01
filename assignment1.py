ed842363e58bd3f9dfeefa5f454ff8f5aa3aeee6
cmis <cmis@cmis-hs01.(none)>
2016-02-01 09:56:04 +0700

    Assignment with Variabl

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+Myname = "DanielKwon"
+Myage = 16
+MyHeight = 170
+LengthS = 1
+LengthR = 1
+HeightR = 1
+MyageMonth = Myage * 12
+Lastmore = 81-Myage
+MyHeightF = 170/30.5
+MyAverageHeight = 173 - MyHeight
+AreaR = LengthS * 2
+VolumeC = (LengthR**3.0)/2.0
+Dividedarea = AreaR/9.0
+print str (Myname) str (LengthS) str (MyHeight) str (Myage)  str (MyHeightF)
