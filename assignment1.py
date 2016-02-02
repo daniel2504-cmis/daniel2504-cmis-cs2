d1a95be7af966c120148b2f46a20b438af608311
cmis <cmis@cmis-hs01.(none)>
2016-02-02 11:41:37 +0700

    This an assignment for values, variables. and math

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,19 @@
-Myname = "DanielKwon"
-Myage = 16
-MyHeight = 170
-LengthS = 1
-LengthR = 1
-HeightR = 1
-MyageMonth = Myage * 12
-Lastmore = 81-Myage
-MyHeightF = 170/30.5
-MyAverageHeight = 173 - MyHeight
-AreaR = LengthS * 2
-VolumeC = (LengthR**3.0)/2.0
-Dividedarea = AreaR/9.0
-print str (Myname) str (LengthS) str (MyHeight) str (Myage)  str (MyHeightF)
+Myname = "DanielKwon" #This is the variable for my name
+Myage = 16 #This is the variable for my age
+MyHeight = 170 #This is the variable for My Height
+LengthS = 1 #This is the variable for Length Square
+LengthR = 1 #This is the variable for Length Rectangle 
+HeightR = 1 #This is the variable for Height Rectangle
+MyageMonth = Myage * 12 #This is the variable for my age in month
+Lastmore = 81-Myage #This is the variable for how long ill live more
+MyHeightF = 170/30.5 #This is the variable for my height in feet
+MyAverageHeight = 173 - MyHeight #This is the variable for difference between average height and my height
+AreaR = LengthS * 2 #This is the variable for Area of Rectangle
+VolumeC = (LengthR**3.0)/2.0 #This is the variable for Volume of Cube
+Dividedarea = AreaR/9.0 #This is the variable for Divded area
+print "Hi, my name is " + str(Myname)  + " Im not really that tall " + str(MyHeight) + " I am currently in highschool " + str(Myage) +  " It is nice to meet you " + str(VolumeC) #This is the variable for 5 vocabes that i used
+print "Hi my name is " + str(Myname) + ". Im not really tall, I am " + str(MyHeight) + ". In feet my height is " + str(MyHeightF) + ". Which is lower " + str(MyAverageHeight) + "cm than korean average height " + ". I am currently at highschool " + str(Myage) + " Years Old " + ". I will live more " + str(Lastmore) + ". This pyhton is made by " + str(Myname) + ". It can also do math, if Length of Rectangle is " + str(LengthS) + " The volume of the cube will be " + str(VolumeC) + ". Also the Area of the rectangle will be " + str(AreaR) + ". It is nice to meet you " #This is the variable for more than 6 vocabes i used
+Wink = ";)" #This is the variable for Wink face
+WinkT = Wink * 10000 #This is the variable for thousand Wink face
+print WinkT #This is the variable to prin thousand Wink face
+"gg"
