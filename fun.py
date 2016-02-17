3aec57eaf730bbeada9c0eb8132363d35cf6c9ee
cmis <cmis@cmis-hs01.(none)>
2016-02-17 09:55:52 +0700

    This is just for fun.

---
+++ fun.py
@@ -0,0 +1,13 @@
+x = raw_input("What's your best champion?: ")
+y = raw_input("Tryn? :")
+z = raw_input("If not, why dont u play tryn?: ")
+
+output = """
+Tryn is love, ur {} is a worst champion ever made.
+You should try tryn "{}" can't be a excuse to deny this beautiful champion
+I assure you "{}" will change as soon you know about tryndamere.
+""". format(x, z, y)
+ 
+print output
+
+Input = 
