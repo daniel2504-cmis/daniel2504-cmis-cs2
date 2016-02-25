07bfb817dd3275839578011a2ad0f29880926147
cmis <cmis@cmis-hs01.(none)>
2016-02-25 09:54:59 +0700

    Now starting :(

--- simple.py
+++ simple.py
@@ -3,8 +3,21 @@ def add(x,y):
 	return x + y
 
 def output(name, x, y, z):
-	"""
+	a = """
 Hey {}
 Did you know:
 {} + {} = {}
-""". format(Daniel, 1,2, 3) 
+""". format(name, x, y, z) 
+	return a
+
+def main():
+	name = raw_input("What's your name?: ")
+	x = raw_input("Type a number: ")
+	y = raw_input("Type a number: ")
+
+	z = add(int(x), int(y))
+
+	a = output(name, x, y, z)
+	print a
+
+main()
