9b7650bebd8921e3d5a0783cc2a1d325f4932a1e
cmis <cmis@cmis-hs01.(none)>
2016-03-16 11:55:21 +0700

    Sigh

--- oneguess.py
+++ oneguess.py
@@ -10,33 +10,45 @@ def sub(x,y):
 	return x - y
 
 def think(x,y):
-	return random.radint(x,y)
+	return random.randint(x,y)
 
-def what(x):
-	x = return_input("What do you think it is?: x")
+def what():
+	x = raw_input("What do you think it is?: ") 
 	return x
-#Output
-def output():
-	out = """
-I'm thinking of a number from {} to {}.
+	
 
-The target was {}.
-Your guess was {}.
-That's under by {}""".format()
 
+def theflipman():
+	minimum = raw_input("What is the minimum number?:")
+	maximum = raw_input("What is the maximum number?:")
+	return minimum, maximum
+
+def mydays(): 
+	Hmm = think(int(minimum), int(maximum))
+	return """
+	I'm thinking of a number from {} to {}.""". format(minimum, maximum)
+
+def correcting(x):
+	x = what()
+	if x > think(int(minimum), int(maximum)):
+		False
+	elif x < think(int(minimum), int(maximum)):
+		False
+	else:
+		return x
 #Main
 def main():
 	#Input Section
-	minimum = raw_input("What is the minimum number?:")
-	maximum = raw_input("What is the maximum number?:")
+	theflipman()
+	print mydays
+	reply = what()
 
 	#Processing
-	Hmm = think(int(minimum), int(maximum))
-	what(x)
-	
+	correcting(int(reply))
+
 	#Output
-	out = output()
-	print out
+
+	
 
 
 #Call Main
