348425554ac3406a88c5d8d733a595db3d34e48a
cmis <cmis@cmis-hs12.(none)>
2016-03-17 14:40:14 +0700

    Almost there

--- oneguess.py
+++ oneguess.py
@@ -12,48 +12,34 @@ def sub(x,y):
 def think(x,y):
 	return random.randint(x,y)
 
-def what():
-	x = raw_input("What do you think it is?: ") 
-	return x
+def output(answer, guess, tada):
+	output = """
+The target was {}.
+Your guess was {}.
+That's under by {}.""". format(answer,guess,tada)
+		return output
 	
-
-
-def theflipman():
-	minimum = raw_input("What is the minimum number?:")
-	maximum = raw_input("What is the maximum number?:")
-	return minimum, maximum
-
-def mydays(): 
-	Hmm = think(int(minimum), int(maximum))
-	return """
-	I'm thinking of a number from {} to {}.""". format(minimum, maximum)
-
-def correcting(x):
-	x = what()
-	if x > think(int(minimum), int(maximum)):
-		False
-	elif x < think(int(minimum), int(maximum)):
-		False
-	else:
-		return x
 #Main
 def main():
 	#Input Section
-	theflipman()
-	print mydays
-	reply = what()
-
-	#Processing
-	correcting(int(reply))
-
-	#Output
-
+	minimum = raw_input("What is the minimum number?:")
+	maximum = raw_input("What is the maximum number?:")
+	print "I'm thinking of a number from " + minimum + " to " + maximum+ "."
 	
 
+	#Processing
+	guess = int(raw_input("What do you think it is?: "))
+	if 
+	False = guess < int(minimum)
+	elif
+	False = guess > int(maximum)
+	else
+	True
+	answer = think(int(minimum),int(maximum))
+	tada = sub(answer, guess)
+
+	#Output
+	print output(answer, guess, tada)
 
 #Call Main
 main()
-	
-	
-
-
