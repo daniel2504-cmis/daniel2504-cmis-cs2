b9ffbeb3a76e855c13fdf64c96fd3f9a1e5c4574
cmis <cmis@cmis-hs12.(none)>
2016-03-24 14:06:35 +0700

    Doing

---
+++ conditionals2.py
@@ -0,0 +1,14 @@
+#In this game, you are a detective who is trying to find the murderer who killed Arthas. The killer could be one of the five people who was with Arthas at home. You are given three chances to find any evidences and afterward you will need to choose one of the five people that will be the criminal. However, there are many obstacles while your finding the evidence such as solving problems, random luck, and persuading people to talk. Hopefully, you solve this mysterious crime as a great detective.
+
+#import
+import math
+import random
+
+#Proluge
+print "12:00 am, Chiang Mai Thailand, small house. " + "Cop: Good Morning Detective it is nice to see you."                                                                          "You: Good Morning, can you tell me any information before I start to investigate?"
+print "Yes sir, the murderer is a clever one, we can tell that he was planning for this murder for a long time, we couldn't find any evidences. The person who was killed is Arthas. We found his body yesterday 5:00 am and he was killed by a knife getting through his throat. His body was very warm, so we could tell it didn't been that long after he died."
+print "Anything can be tricked, don't decide like that."
+print "Sorry sir, anyways that was everything we could find, is there anything you want to ask?"
+print "Who was with him at house?"
+print "There was five people. His father, Noah Ma, his old friend, Jaina Proudmoore, the homeowner, Steven Lee, his girlfriend, Emma Skywalker, and plumber, Dawit Cho. Five people looks all inocent."
+print "Ofcourse, they are. Lets go look at them."
