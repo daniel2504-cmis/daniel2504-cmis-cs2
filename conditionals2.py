0485ba10ea2b7a0a5bf9d83ef749cb950e80fe2a
cmis <cmis@cmis-hs12.(none)>
2016-03-29 09:57:03 +0700

    AOAOWOW

--- conditionals2.py
+++ conditionals2.py
@@ -5,10 +5,57 @@ import math
 import random
 
 #Proluge
-print "12:00 am, Chiang Mai Thailand, small house. " + "Cop: Good Morning Detective it is nice to see you."                                                                          "You: Good Morning, can you tell me any information before I start to investigate?"
-print "Yes sir, the murderer is a clever one, we can tell that he was planning for this murder for a long time, we couldn't find any evidences. The person who was killed is Arthas. We found his body yesterday 5:00 am and he was killed by a knife getting through his throat. His body was very warm, so we could tell it didn't been that long after he died."
-print "Anything can be tricked, don't decide like that."
-print "Sorry sir, anyways that was everything we could find, is there anything you want to ask?"
-print "Who was with him at house?"
-print "There was five people. His father, Noah Ma, his old friend, Jaina Proudmoore, the homeowner, Steven Lee, his girlfriend, Emma Skywalker, and plumber, Dawit Cho. Five people looks all inocent."
-print "Ofcourse, they are. Lets go look at them."
+print """12:00 am, Chiang Mai Thailand, small house.
+Cop: "Good Morning Detective it is nice to see you."                                                                                                                                               You: "Good Morning, can you tell me any information before I start to investigate?"
+Cop: "Yes sir, the murderer is a clever one we couldn't find any evidences. The person who was killed is Arthas. We found his body yesterday 5:00 am and he was killed by a knife getting through his throat. His body was very warm, so we could tell it didn't been that long after he died."
+You : "Who was with him at house?"
+
+Cop: There was five people. His father, Noah Ma, his old friend, Jaina Proudmoore, the homeowner, Steven Lee, his girlfriend, Emma Skywalker, and plumber, Dawit Cho. Five people looks all inocent."
+You: "Ofcourse, they are. Lets go look at them."
+
+At night, you need to decide who is the killer and you have three chances to find evidences, you can ask two people and go to one location. Good luck!"""
+
+#Game Starts! Import
+import math
+import random
+
+#Functions
+
+def Ask(x):
+	w
+def deadtrap(x):
+	x = raw_input("Hey way are u going! you gotta play the game!(Yes or No):")
+	if x = "No"
+	print "Well gg"
+	else: print "Anyways, I'm gonna kill you, gg."
+	
+def Noah(x):
+	x = random.randint(1,10)
+	if x < 5
+	print "Im very sorry, I'm too depressed."
+	else: t = raw_input("What do you want to know?(Can you tell me anything that happen? or What were you doing?")
+	if t = "What do you want to know?"
+	print "I remember that it was a rainy day and I couldn't hear anythin when I was sleeping at the first floor. This house has second floor."
+
+def Dawit(x):
+	x = raw_input("Im just a plumber there is nothing I can see about this murder(But you were at house or Please we really need your help:")
+	if x = "But you were at house"
+	print "Fine, I can tell you that I was at the second floor trying to fix the A.C. I managed to fix it at 3:00 am and sleep at the house, because it was raining."
+	else: print "Well I don't care, go away."
+
+def Jaina(x):
+	x = raw_input("Poor Arthas...is there anything I can help?(Please tells us what happen or nope I don't need you")
+	if x = "nope I don't need you"
+	print "Why do you need to be so rude~"
+	else : print "I only remember that A.C turned on at 3:00am, becuase I suddenly felt hot."
+	 
+def Emma(x):
+	print "She is crying so hard that I couldn't ask anything."
+
+def Cop(x):
+	print """You: Where were the people sleeping?
+Cop: The father and Emma were sleeping at the first floor, rest of them were at the second floor."""
+
+
+	
+
