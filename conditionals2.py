6f1be5d5ddcea81bd6229046a45931f88082b0ea
cmis <cmis@cmis-hs12.(none)>
2016-03-30 11:16:11 +0700

    So Hard

--- conditionals2.py
+++ conditionals2.py
@@ -1,5 +1,15 @@
 #In this game, you are a detective who is trying to find the murderer who killed Arthas. The killer could be one of the five people who was with Arthas at home. You are given three chances to find any evidences and afterward you will need to choose one of the five people that will be the criminal. However, there are many obstacles while your finding the evidence such as solving problems, random luck, and persuading people to talk. Hopefully, you solve this mysterious crime as a great detective.
 
+#Actual Story: The cop was an old friend of Arthas, he planned to kill him for a long time. He came house at 3:00am and turned on the Heater to make the body died at 5:00am. Afterward, he camebacks at 5:00am, pretending he was like coming for research and actually removed all the evidence. For that reason, If you go to the police car, there is the knife that killed Arthas. The cop said there was no evidence at the first, which means it was lie. In A. C room, there is plumber's checklist that he fixed at 3:00am persuaded to the cop that it is very cold. (Nobody could here Arthas screaming, because it was raining) In Arthas's room, you can suprisingly see a picture that there is Arthas and fat man, but the face is ripped up. (The cop removed his picture to make like he never knew Arthas)(From here you can find that the criminal is one of the male, not female).  
+
+#For asking people.
+#Steven : very helpful to find that cop is murder
+#Noah : A little helpful if you understand what it means.
+#Dawit: Makes a lot of confusion
+#Jaina : less helpful
+#Emma : Useless
+#Cop : Can know the locations. (But he never tells he was in house at 3:00 am)
+
 #import
 import math
 import random
@@ -22,32 +32,55 @@ import random
 #Functions
 
 def Ask(x):
-	w
+	
+
+def location(x):
+	
+
 def deadtrap(x):
 	x = raw_input("Hey way are u going! you gotta play the game!(Yes or No):")
 	if x = "No"
 	print "Well gg"
+	exit()
 	else: print "Anyways, I'm gonna kill you, gg."
+	exit()
 	
+def ACRoom(x):
+	x = raw_input("There is a password to go in (0.0to1.0):")
+	y = random.random()
+	if int(x) = int(y) 
+	print "You went in and found Plumber's checklist. It says that the cop asked him to fix the A.C. Interesting..."
+	else : print "fail"
+
+def ArthasRoom(x)
+	x = raw_input("THere was nothing special except a bed and picture(look picture or look bed)
+	if x = "bed"
+	print "Nothing was there"
+	else : print "In the picture, there was Arthas and a man, whose face was ripped off. Hmmm I wonder why it is removed.")
+
+def Policecar(x):
+	print "Near the car, you found a knife."
+
 def Noah(x):
 	x = random.randint(1,10)
 	if x < 5
 	print "Im very sorry, I'm too depressed."
 	else: t = raw_input("What do you want to know?(Can you tell me anything that happen? or What were you doing?")
 	if t = "What do you want to know?"
-	print "I remember that it was a rainy day and I couldn't hear anythin when I was sleeping at the first floor. This house has second floor."
+	print "I remember that it was a rainy day and I could hear someone open the door at 3:00am. This house has second floor."
 
 def Dawit(x):
 	x = raw_input("Im just a plumber there is nothing I can see about this murder(But you were at house or Please we really need your help:")
 	if x = "But you were at house"
-	print "Fine, I can tell you that I was at the second floor trying to fix the A.C. I managed to fix it at 3:00 am and sleep at the house, because it was raining."
+	print "Fine, Somebody called me and told me to fix the A.C. at 3:00 am and sleep at the house, because it was raining. When I was done, Steven asked me to sleep here, so I just slept."
 	else: print "Well I don't care, go away."
 
 def Jaina(x):
 	x = raw_input("Poor Arthas...is there anything I can help?(Please tells us what happen or nope I don't need you")
 	if x = "nope I don't need you"
 	print "Why do you need to be so rude~"
-	else : print "I only remember that A.C turned on at 3:00am, becuase I suddenly felt hot."
+	exit()
+	else : print "At 5:00am I came up to wake up Arthas for a question. However, I found him dead..."
 	 
 def Emma(x):
 	print "She is crying so hard that I couldn't ask anything."
@@ -56,6 +89,21 @@ def Cop(x):
 	print """You: Where were the people sleeping?
 Cop: The father and Emma were sleeping at the first floor, rest of them were at the second floor."""
 
+def Steven(x):
+	x = raw_input("Yes, how could I help you?(Anything you know or I have a feeling that the gamemaker hates you"):
+	if x = "I have a feeling that the gamemaker hates you"
+	print "It is true"
+	exit()
+	else : print "I remember that cop came to this house at 3:00am to check tax.")
 
+#Main
+main():
+	#Input
+	x1 = raw_input("Who will you ask?(Option: Noah, Dawit, Jaina, Emma, Steven, and Cop):")
+	x2 = raw_input("Who will you ask?(Option: Noah, Dawit, Jaina, Emma, Steven, and Cop):")
+	y = raw_input("Where will you go?(Option: Police Car, Screw this game, Arthas's room, and A.C. Room: ")
+
+	process:
+	
 	
 
