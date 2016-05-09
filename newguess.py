1205c8bf14ad4ace1ad4ec4d402ddf75439c2bae
cmis <cmis@cmis-hs12.(none)>
2016-05-09 14:00:31 +0700

    Yeah Im done

--- newguess.py
+++ newguess.py
@@ -1,28 +1,29 @@
 #import random
 import random
 
-def guess(tell,roundnumber,answer,wins,chances):
+def guess(tell,roundnumber,answer,wins,chances,rounds):
 	print answer
 	if chances == 0:
 		print "Your not really good at this game"
-		guess2(roundnumber,answer,wins)
+		guess2(roundnumber-1,answer,wins,rounds-1)
 	elif tell < answer:
 		print "Too low!"
-		guess(int(raw_input("Your guess:")),roundnumber,answer,wins,chances-1)
+		guess(int(raw_input("Your guess:")),roundnumber,answer,wins,chances-1,rounds)
 	elif tell > answer:
 		print "Too high!" 
-		guess(int(raw_input("Your guess:")),roundnumber,answer,wins,chances-1)
+		guess(int(raw_input("Your guess:")),roundnumber,answer,wins,chances-1,rounds)
 	elif tell == answer:
 		print "Correct!"
-		print "Round" + str(wins+1)
-		guess(int(raw_input("Your guess:")),answer,random.randint(0,100),wins+1,4)
+		print "Round" + str(rounds+1)
+		guess(int(raw_input("Your guess:")),roundnumber,random.randint(0,100),wins+1,4,rounds+1)
 
-def guess2(x,y,c):
-	if x == 0:
-		print str(y)
+def guess2(x,y,c,e):
+	if e == 0:
+		print str(c)
 		exit()
 	else: 
-		print "Round" + str(wins-1)
-		returnguess(int(raw_input("Your guess:")),roundnumber-1,random.randint(0,100),c,4)
+		print "Round" + str(x)
+		return guess(int(raw_input("Your guess:")),x,random.randint(0,100),c,4,e)
+
 print "Round 1"
-guess(int(raw_input("Your guess:")),2,random.randint(0,100),0,4)
+guess(int(raw_input("Your guess:")),2,random.randint(0,100),0,4,1)
