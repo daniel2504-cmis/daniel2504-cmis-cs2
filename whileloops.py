4a4069be6b66b5ec6bf95c01d3956e5d8686347f
cmis <cmis@cmis-hs12.(none)>
2016-05-26 14:37:06 +0700

    d

--- whileloops.py
+++ whileloops.py
@@ -1,6 +1,6 @@
 
-# x = 10
-# while x >= 0:
+#x = 10
+#while x >= 0:
 #	print x
 #	x += -1
 
@@ -18,8 +18,48 @@ def counter(x):
 		while x < 1:
 			print x
 			x += 1
-
 	else:
 		print x
 
-counter(int(raw_input("N:")))
+#counter(int(raw_input("N:")))
+
+def counter2(x,y):
+	if x > 0:
+		while x >= y:
+			print x
+			x += -1
+	elif x < 0:
+		while x < y:
+			print x
+			x += 1
+	else:
+		print x
+
+#counter2(-10,10)
+
+def sumofodd(x):
+	total = 0
+	if x > 0:
+		while x >= 0:
+			print x
+			if x % 2 != 0:
+				total += x
+				x += -1
+			elif x == 0:
+				print total
+			else:
+				x += -1		
+	elif x < 0:
+		while x < 0:
+			print x
+			if x % 2 != 0:
+				total += x
+				x += 1
+			elif x == 0:
+				print total
+			else:
+				x += -1	
+	else:
+		print total
+
+sumofodd(10)
