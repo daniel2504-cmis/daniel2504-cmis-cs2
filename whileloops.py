dcb9a689524f988bec9f8bb99150525df187ae1f
cmis <cmis@cmis-hs12.(none)>
2016-05-30 09:55:24 +0700

    f

--- whileloops.py
+++ whileloops.py
@@ -39,27 +39,35 @@ def counter2(x,y):
 
 def sumofodd(x):
 	total = 0
-	if x > 0:
+	if float(x) > 0:
 		while x >= 0:
-			print x
 			if x % 2 != 0:
 				total += x
 				x += -1
 			elif x == 0:
 				print total
+				exit()
 			else:
 				x += -1		
-	elif x < 0:
-		while x < 0:
-			print x
+	else:
+		while x <= 0:
 			if x % 2 != 0:
 				total += x
 				x += 1
 			elif x == 0:
 				print total
+				exit()
 			else:
-				x += -1	
-	else:
-		print total
+				x += 1	
 
-sumofodd(10)
+
+#sumofodd(13)
+
+def grid(w,h):
+	out = ""
+	while w > 0:
+		out += "."
+		w -= 1
+		return out
+	
+print grid(5,5)	
